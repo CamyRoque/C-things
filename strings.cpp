968849c88d1b4d938c2941a986d47a2e66b89397
CamyRoque <59942072+CamyRoque@users.noreply.github.com>
2020-01-23 22:13:39 -0300

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	//definindo uma string
+	string palavra;
+	
+	//imprimindo na tela
+	cout << "Digite uma palavra";
+	
+	//lendo uma string
+	cin >> palavra;
+	
+	//imprimindo uma variável
+	cout << "\n a palavra é:" << palavra;
+
+	
+	return 0;
+}
